e084df44d6cdf98b874161631996f15298368f5f
Roy Zhu <roynorthyork@yahoo.com>
2018-10-22 17:40:29 -0400

    fix code after checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  
+  /**
+   * @author zhaohua
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
