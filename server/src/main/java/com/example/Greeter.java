c29a01fd3f6e853a60bfedd2e32f334daf83566f
Roy Zhu <roynorthyork@yahoo.com>
2018-10-22 18:13:54 -0400

    fix warnings for javadoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
   
   /**
-   * @param Roy zhu
+   * @param someone the name of person
    * @return greeting string
    */
   public final String greet(final String someone) {
